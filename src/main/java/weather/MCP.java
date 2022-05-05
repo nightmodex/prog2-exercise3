c44d9e307a1ba6cb64bdff53c123d0303c1d87ef
nightmodex <thomas.noori@gmail.com>
2022-05-05 09:32:15 +0200

    added git repo link

--- src/main/java/weather/MCP.java
+++ src/main/java/weather/MCP.java
@@ -5,6 +5,7 @@ import weather.ui.UserInterface;
 public class MCP {
 
     public static void main(String[] args) {
+        //https://github.com/nightmodex/prog2-exercise3.git
     	UserInterface ui = new UserInterface();
     	ui.start();
     }
